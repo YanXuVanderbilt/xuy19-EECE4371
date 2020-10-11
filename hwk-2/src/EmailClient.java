63b504500b2a6d5a7db8ea5902a2901a0ffabd5b
xuyan <yan.xu@vanderbilt.edu>
2020-10-11 12:21:06 -0500

    some progress

--- hwk-2/src/EmailClient.java
+++ hwk-2/src/EmailClient.java
@@ -1,3 +1,4 @@
+import javax.management.NotificationEmitter;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -9,11 +10,44 @@ public class EmailClient {
     private static final String HOST_ADDRESS = "127.0.0.1";
     //private static final String HOST_ADDRESS = "10.66.56.222";
     private static final int PORT = 6789;
+    private static String userName = "";
+    private static final BufferedReader userBufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public EmailClient() throws IOException {
+    }
+
+    public static int displayMenu() throws IOException {
+        System.out.println("1. Send Mail");
+        System.out.println("2. Read Mail");
+        System.out.println("3. Exit");
+        //System.out.flush();
+        //Scanner in = new Scanner(System.in);
+        String userInput = userBufferedReader.readLine();
+        //String userInput = in.nextLine();
+        //System.out.println(userInput);
+        //in.close();
+        switch (userInput) {
+            case "1" -> {
+                System.out.println("client:displayMenu:debug:send mail");
+                return 1;
+            }
+            case "2" -> {
+                System.out.println("client:displayMenu:debug:read mail");
+                return 2;
+            }
+            case "3" -> {
+                System.out.println("client:displayMenu:debug:exit");
+                return 3;
+            }
+            default -> {
+                System.out.println("please enter 1, 2, or 3");
+                return displayMenu();
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
-        //login();
-        //sendEmail();
-        EmailProtocolMessage msg = new EmailProtocolMessage("a;b;c;d");
         Socket socket = new Socket(HOST_ADDRESS, PORT);
         DataOutputStream outToServer= new DataOutputStream(socket.getOutputStream());
         BufferedReader serverBufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -23,48 +57,116 @@ public class EmailClient {
 
         boolean quit = false;
 
-        System.out.println("Please enter a message to send");
-        BufferedReader userBufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String userInput = userBufferedReader.readLine();
-        outToServer.writeBytes("type=log_in&username=yanxu" + "\n");
-        outToServer.writeBytes("type=send_email&email=to>jbeck;from>yanxu;body>give me extra credit" + "\n");
+        outToServer.writeBytes(login() + "\n");
+        String login_ack = serverBufferedReader.readLine();
+        System.out.println(login_ack);
+        if (!login_ack(login_ack)) {
+            System.out.println("No response from server. Login failed.\n");
+            return;
+        }
+        while (!quit) {
+            int choice = displayMenu();
+            switch (choice) {
+                case 1 -> outToServer.writeBytes(sendEmail() + "\n");
+                case 2 -> outToServer.writeBytes(retrieve() + "\n");
+                case 3 -> outToServer.writeBytes(logOut() + "\n");
+                default -> {
+                    System.out.println("Something is very wrong here. Quitting.\n");
+                    quit = true;
+                }
+            }
+            if (choice == 3) {
+                quit = true;
+            }
 
 
-        String responseFromServer = serverBufferedReader.readLine();
-        System.out.println("Response from server: " + responseFromServer);
+            //BufferedReader userBufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            //String userInput = userBufferedReader.readLine();
+            //outToServer.writeBytes("type=log_in&username=yanxu" + "\n");
+            //outToServer.writeBytes("type=send_email&email=to>jbeck;from>yanxu;body>give me extra credit" + "\n");
+            //outToServer.writeBytes(userInput+"\n");
 
+            String responseFromServer = serverBufferedReader.readLine();
+            System.out.println("Response from server: " + responseFromServer);
 
+            if (choice == 2) {
+                displayEmails(responseFromServer);
+            }
+        }
 
-        //socket.close();
+        socket.close();
     }
 
-    private static int login() {
+    private static String login() throws IOException {
         String usrName;
-        Scanner in = new Scanner(System.in);
+       // Scanner in = new Scanner(System.in);
 
         System.out.print("Please provide your username: ");
-        usrName = in.nextLine();
-        System.out.print(usrName);
-        return 0;
+        //usrName = in.nextLine();
+        usrName = userBufferedReader.readLine();
+        userName = usrName;
+        EmailProtocolMessage login_msg = new EmailProtocolMessage();
+        login_msg.putParam(EmailProtocolMessage.TYPE_KEY, EmailProtocolMessage.LOGIN_COMMAND);
+        login_msg.putParam(EmailProtocolMessage.USERNAME_KEY, userName);
+        //in.close();
+        return login_msg.toString();
     }
 
-    private static int sendEmail() {
+    private static String sendEmail() throws IOException {
+        if (userName.equals("")) {
+            return ""; // not logged in
+        }
         String recipient;
         String body;
-        Scanner in = new Scanner(System.in);
+        //Scanner in = new Scanner(System.in);
         System.out.print("Who are you sending to? ");
-        recipient = in.nextLine();
+        //recipient =
+        recipient = userBufferedReader.readLine();
         System.out.println("Please enter the body of your email:");
-        body = in.nextLine();
-        System.out.print(recipient+body);
-        return 0;
+        //body = in.nextLine();
+        body = userBufferedReader.readLine();
+        //System.out.print(recipient+body);
+        String email = "to>" + recipient + ";from>" + userName + ";body>" + body;
+        EmailProtocolMessage msg = new EmailProtocolMessage();
+        msg.putParam(EmailProtocolMessage.TYPE_KEY, EmailProtocolMessage.SENDEMAIL_COMMAND);
+        msg.putParam(EmailProtocolMessage.EMAIL_KEY, email);
+        //in.close();
+        return msg.toString();
     }
 
-    private static int retrieve() {
-        return 0;
+    private static String retrieve() {
+        EmailProtocolMessage msg = new EmailProtocolMessage();
+        msg.putParam(EmailProtocolMessage.TYPE_KEY, EmailProtocolMessage.RETRIVE_EMAIL_COMMAND);
+        return msg.toString();
     }
 
-    private static int logOut() {
-        return 0;
+    private static void displayEmail(String in) {
+        String[] fields = in.split(";");
+        for (String field : fields) {
+            System.out.println(field);
+        }
+        System.out.println();
+    }
+
+    private static void displayEmails(String in) {
+        EmailProtocolMessage msg = new EmailProtocolMessage(in);
+        String emails_str = msg.getParam(EmailProtocolMessage.EMAILS_KEY);
+        String[] emails = emails_str.split("ZZZ");
+        for (String email : emails) {
+            displayEmail(email);
+        }
+        System.out.println("client:displayEmails:debug:exited successfully");
+    }
+
+    private static String logOut() {
+        EmailProtocolMessage msg = new EmailProtocolMessage();
+        msg.putParam(EmailProtocolMessage.TYPE_KEY, EmailProtocolMessage.LOGOUT_COMMAND);
+        return msg.toString();
+    }
+
+    private static boolean login_ack(String ack) {
+        EmailProtocolMessage msg = new EmailProtocolMessage(ack);
+        //System.out.println("client:login_ack:debug:" + msg.getParam(EmailProtocolMessage.STATUS_KEY));
+        return msg.getParam(EmailProtocolMessage.STATUS_KEY).equals(EmailProtocolMessage.OK_STATUS);
     }
 }
--- hwk-2/src/EmailServer.java
+++ hwk-2/src/EmailServer.java
@@ -5,10 +5,13 @@ import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class EmailServer {
-    private ServerSocket mServerSocket;
+    private final ServerSocket mServerSocket;
     private Socket mClientSocket;
+    private boolean isLoggedIn = false;
+    private final ArrayList<String> mEmails = new ArrayList<>();
 
     public EmailServer(int port) throws IOException {
         mServerSocket = new ServerSocket(port);
@@ -23,35 +26,44 @@ public class EmailServer {
             DataOutputStream clientWriter = new DataOutputStream(mClientSocket.getOutputStream());
 
             String clientMessage = clientReader.readLine();
+            if (clientMessage == null) {
+                continue;
+            }
             System.out.println("received message: " + clientMessage);
 
-            /*
             EmailProtocolMessage message = new EmailProtocolMessage(clientMessage);
+            String response;
+
 
             switch (message.getParam(EmailProtocolMessage.TYPE_KEY)) {
-                case EmailProtocolMessage.LOGIN_COMMAND:
-                    login();
-                    break;
-                case EmailProtocolMessage.SENDEMAIL_COMMAND:
-                    sendEmail();
-                    break;
-                case EmailProtocolMessage.RETRIVE_EMAIL_COMMAND:
-                    retrieve();
-                    break;
-                case EmailProtocolMessage.LOGOUT_COMMAND:
-                    logOut();
-                    break;
-                default:
+                case EmailProtocolMessage.LOGIN_COMMAND -> {
+                    response = login();
+                    System.out.println("server: debug: logged the user in");
+                }
+                case EmailProtocolMessage.SENDEMAIL_COMMAND -> {
+                    response = sendEmail(message);
+                    System.out.println("server: debug: sent email");
+                }
+                case EmailProtocolMessage.RETRIVE_EMAIL_COMMAND -> {
+                    response = retrieve();
+                    System.out.println("server: debug: retrieved email");
+                }
+                case EmailProtocolMessage.LOGOUT_COMMAND -> {
+                    response = logOut();
+                    System.out.println("server: debug: logged user out");
+                }
+                default -> {
+                    System.out.println("Error in command");
+                    response = "ERROR";
+                }
             }
-            */
+
             if (clientMessage.equals("quit")) {
                 quit = true;
             }
 
-            String response = "You sent me " + clientMessage.length() + " characters\n";
-            clientWriter.writeBytes(response);
-
-            System.out.println("");
+            clientWriter.writeBytes(response + "\n");
+            System.out.println("written response to user: " + response + "\n");
         }
     }
 
@@ -63,20 +75,44 @@ public class EmailServer {
         System.out.println("client at: " + clientAddress.toString() + ":" + mClientSocket.getPort());
     }
 
-    private int login() {
-        return 0;
+    private String login() {
+        isLoggedIn = true;
+        return login_ack();
     }
 
-    private int sendEmail() {
-        return 0;
+    private static String login_ack() {
+        EmailProtocolMessage msg = new EmailProtocolMessage();
+        msg.putParam(EmailProtocolMessage.TYPE_KEY, EmailProtocolMessage.LOGIN_ACK);
+        msg.putParam(EmailProtocolMessage.STATUS_KEY, EmailProtocolMessage.OK_STATUS);
+        return msg.toString();
     }
 
-    private int retrieve() {
-        return 0;
+    private String sendEmail(EmailProtocolMessage msg) {
+        if (!isLoggedIn) {
+            return "server:sendEmail:debug:ERRRO: User is not logged in";
+        }
+        mEmails.add(msg.getParam(EmailProtocolMessage.EMAIL_KEY));
+        return "server:sendEmail:debug:Email sent";
     }
 
-    private int logOut() {
-        return 0;
+    private String retrieve() {
+        if (!isLoggedIn) {
+            return "server:retrieve:debug:ERROR: User is not logged in";
+        }
+        String emails = String.join("ZZZ", mEmails);
+        EmailProtocolMessage msg = new EmailProtocolMessage();
+        msg.putParam(EmailProtocolMessage.TYPE_KEY, EmailProtocolMessage.EMAILS_KEY);
+        msg.putParam(EmailProtocolMessage.EMAILS_KEY, emails);
+        System.out.println("server:retrieve:debug:" + msg.toString());
+        return msg.toString();
+    }
+
+    private String logOut() {
+        if (!isLoggedIn) {
+            return "server:logOut:debug:ERRRO: User is not logged in";
+        }
+        isLoggedIn = false;
+        return "server:logOut:debug:logged user out";
     }
 
     private static final int SERVER_PORT = 6789;
